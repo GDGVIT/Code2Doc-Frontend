b7a794d30ff82bdf1a1e8f0b1b302de2f919403e
anirudh <anirudh04mishra@gmail.com>
2022-05-27 12:31:27 +0530

    feat: initialise svelte project

---
+++ project/src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { onMount } from 'svelte';
+
+  let userID;
+
+  onMount(async () => {
+    userID = 'adsw8239nssj';
+  });
+</script>
+
+<main>
+  <h1>Welcome to Code2Doc!</h1>
+  <p>User ID: {userID}</p>
+</main>
+
+<style>
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
