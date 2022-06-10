2b0014d1aef5b930676a292b7e553d348413a9a8
anirudh <anirudh04mishra@gmail.com>
2022-06-10 10:56:18 +0530

    feat: responsiveness and tweaks

--- project/src/App.svelte
+++ project/src/App.svelte
@@ -2,14 +2,13 @@
   import '../scripts/registerServiceWorker';
   import { onMount, onDestroy } from 'svelte';
   import axios from 'axios';
-  import Dropzone from 'dropzone';
   import 'primeflex/primeflex.css';
 
   let userID;
   let processed = false;
   let uploaded = false;
   let downloaded = false;
-  let fileFormat = '';
+  let fileFormat = [];
   let uploadedFiles = [];
 
   onMount(async () => {
@@ -20,14 +19,13 @@
       const fileList = this.files;
       for (let i = 0; i < fileList.length; i++) {
         uploadedFiles.push(fileList[i]);
-        console.log(fileList[i]);
+        fileFormat.push(fileList[i].name.split('.').pop());
         uploadedFiles = uploadedFiles;
       }
     }
     const uploadButtons = document.querySelectorAll('.upload');
     uploadButtons.forEach((button) => {
       button.addEventListener('change', handleFiles, false);
-      console.log('ok');
     });
   });
 
@@ -65,90 +63,107 @@
   };
 
   function dropHandler(ev) {
-    console.log('File(s) dropped');
-
-    // Prevent default behavior (Prevent file from being opened)
     ev.preventDefault();
-
+    var dropzone = ev.currentTarget;
+    dropzone.classList.remove('hovered');
     if (ev.dataTransfer.items) {
-      // Use DataTransferItemList interface to access the file(s)
       for (var i = 0; i < ev.dataTransfer.items.length; i++) {
-        // If dropped items aren't files, reject them
         if (ev.dataTransfer.items[i].kind === 'file') {
           var file = ev.dataTransfer.items[i].getAsFile();
-          console.log('... file[' + i + '].name = ' + file.name);
           uploadedFiles.push(file);
+          fileFormat.push(file.name.split('.').pop());
           uploadedFiles = uploadedFiles;
         }
       }
     } else {
-      // Use DataTransfer interface to access the file(s)
       for (var i = 0; i < ev.dataTransfer.files.length; i++) {
-        console.log(
-          '... file[' + i + '].name = ' + ev.dataTransfer.files[i].name
-        );
         uploadedFiles.push(ev.dataTransfer.files[i]);
+        fileFormat.push(ev.dataTransfer.files[i].name.split('.').pop());
         uploadedFiles = uploadedFiles;
       }
     }
   }
 
   function dragOverHandler(ev) {
-    console.log('File(s) in drop zone');
-    // Prevent default behavior (Prevent file from being opened)
     ev.preventDefault();
+    var dropzone = ev.currentTarget;
+    dropzone.classList.add('hovered');
+  }
+
+  function dragOutHandler(ev) {
+    ev.preventDefault();
+    var dropzone = ev.currentTarget;
+    dropzone.classList.remove('hovered');
   }
 
   $: if (uploadedFiles.length) {
     uploaded = true;
+    console.log(uploadedFiles);
+    console.log(fileFormat);
   } else {
     uploaded = false;
   }
 </script>
 
 <main>
-  <header class="px-5 py-4 flex flex-row align-items-center gap-3">
+  <header class="px-5 py-4 flex flex-row align-items-center gap-2">
     <img src="./logo.svg" class="h-3rem" alt="logo" />
     <h2>code2doc</h2>
   </header>
   <div class="content px text-center">
-    <h1 class="lg:text-8xl md:text-7xl text-6xl mb-5">code_2_doc</h1>
-    <p class="text-xl mb-6 xl:w-3 lg:w-6 tagline">
+    <h1 class="lg:text-8xl md:text-7xl text-6xl sm:mb-5 mb-4 font-bold">
+      code2doc
+    </h1>
+    <p class="sm:mb-6 mb-5 xl:w-3 lg:w-6 tagline">
       A quick solution to get all your code converted to a document.
     </p>
     <div
       id="drop_zone"
       on:drop={dropHandler}
       on:dragover={dragOverHandler}
-      class="relative border-dashed border-round-md p-5 sm:w-10 w-9 flex flex-row flex-wrap h-25rem mt-1"
+      on:dragleave={dragOutHandler}
+      class="z-1 relative p-5 sm:w-10 w-9 sm:h-25rem sm:max-h-25rem h-15rem max-h-15rem mt-1"
     >
-      {#if !uploaded}
-        <div class="m-auto">
-          <label class="inline-block button" for="uploadmain"
-            >Choose files</label
-          >
-          <input type="file" id="uploadmain" class="upload" multiple />
-          <p class="mt-2">or drop your files here</p>
-        </div>
-      {:else}
-        {uploadedFiles.length}
-      {/if}
+      <div class="overflow-auto h-full flex flex-row flex-wrap">
+        {#if !uploaded}
+          <div class="m-auto">
+            <label class="inline-block button" for="uploadmain"
+              >Choose files</label
+            >
+            <input type="file" id="uploadmain" class="upload" multiple />
+            <p class="mt-2">or drop your files here</p>
+          </div>
+        {:else}
+          {#each uploadedFiles as file, i}
+            <div class="xl:w-2 lg:w-3 md:w-4 sm:w-6 w-12">
+              <p>{file.name}</p>
+            </div>
+          {/each}
+        {/if}
+      </div>
       <div
-        style="top: -20px; right: -20px"
-        class="absolute {!uploaded ? 'hidden' : null}"
+        style="top: -25px; right: -25px"
+        class="z-2 absolute {!uploaded ? 'hidden' : null}"
       >
         <label
-          class="inline-block button button-circle text-center"
-          for="upload">+</label
+          class="inline-block button button-circle text-center flex"
+          for="upload"
+        >
+          <img class="m-auto" src="./plus.svg" alt="plus" /></label
         >
         <input type="file" id="upload" class="upload" multiple />
       </div>
     </div>
-    <button class="mt-7 button button-inverse">Convert</button>
+    <button
+      class="sm:mt-7 mt-6 button button-inverse {!uploaded
+        ? 'button-disabled'
+        : null}"
+      disabled={!uploaded}>Convert</button
+    >
   </div>
   <footer class="px">
     <p class="text-center">
-      Made with <span style={{ color: '#FF6B6B' }}>&#9829;</span> by GDSC
+      Made with <span style="color:#FF6B6B">&#9829;</span> by GDSC
     </p>
   </footer>
 </main>
@@ -158,6 +173,10 @@
     main {
       max-width: none;
     }
+  }
+
+  header > h2 {
+    font-family: 'SometypeMono Bold';
   }
 
   .px {
@@ -186,9 +205,5 @@
   h1 {
     font-size: 4rem;
     color: var(--green);
-  }
-
-  #drop_zone {
-    border-color: var(--primary);
   }
 </style>
